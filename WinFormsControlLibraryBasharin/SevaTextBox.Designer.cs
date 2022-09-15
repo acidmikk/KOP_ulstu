4a72faf4d159e81e40e80ed89317da3db5f971c2
acidmikk <sv.acid6@gmail.com>
2022-09-16 01:43:03 +0400

    re_create add clb control add tb control

---
+++ WinFormsControlLibraryBasharin/SevaTextBox.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace WinFormsControlLibraryBasharin
+{
+    partial class SevaTextBox
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(239, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(3, 32);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(116, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Не надо вводить";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Верно";
+            // 
+            // SevaTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "SevaTextBox";
+            this.Size = new System.Drawing.Size(242, 80);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private CheckBox checkBox1;
+        private Label label1;
+    }
+}
---
+++ WinFormsControlLibraryBasharin/SevaTextBox.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsControlLibraryBasharin
+{
+    public partial class SevaTextBox : UserControl
+    {
+        public int MaxLenght { get; set; }
+        public int MinLenght { get; set; }
+        private string text { get; set; }
+        private string Success = "Верно!";
+        private string Wrong = "Не верно!";
+        public SevaTextBox()
+        {
+            InitializeComponent();
+            textBox1.TextChanged += (sender, e) => _event?.Invoke(sender, e);
+        }
+        private event EventHandler _event;
+        public event EventHandler EnterTextChanged
+        {
+            add
+            {
+                _event += value;
+            }
+            remove
+            {
+                _event -= value;
+            }
+        }
+        public string SelectText
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                if (value.Length > MinLenght && value.Length < MaxLenght)
+                    textBox1.Text = value;
+            }
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Enabled = !checkBox1.Checked;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            text = textBox1.Text;
+            if (text.Length > MinLenght && text.Length < MaxLenght)
+            {
+                label1.Text = Success;
+            } else
+            {
+                label1.Text = Wrong;
+            }
+        }
+    }
+}
